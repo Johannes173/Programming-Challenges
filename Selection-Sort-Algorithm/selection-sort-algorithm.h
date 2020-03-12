bc19857b2e84809ecfd36cf79ae1f84649e7c5f8
H4PE0N <h4pe0n.python.3.6@gmail.com>
2020-03-12 14:50:43 +0100

    Created a selection sort algorithm

---
+++ Selection-Sort-Algorithm/selection-sort-algorithm.h
@@ -0,0 +1,13 @@
+
+#ifndef SELECTION_SORT_ALGORITHM_H
+#define SELECTION_SORT_ALGORITHM_H
+
+int switch_lowest_indexes(int*, int, int);
+int generate_lowest_index(int*, int, int);
+
+int* switch_array_values(int*, int, int);
+int* generate_index_number(int*, int, int);
+
+int* selection_sort_array(int*, int);
+
+#endif
